ce3601813a73e0e17585beb127e20017f2cd1f51
Emo-Damage <75520940+Emo-Damage@users.noreply.github.com>
2023-11-05 18:03:21 +0800

    update doc

---
+++  env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			[key: string]: string | undefined;
+			MY_KV_1: KVNamespace;
+			MY_KV_2: KVNamespace;
+			MY_R2: R2Bucket;
+			MY_DO: DurableObjectNamespace;
+		}
+	}
+}
+
+export {};
